c3fadedac8a811dd4730a3a8e4676ac52f8e7b45
Chiu Lam <happylam23@gmail.com>
2025-03-03 19:31:56 +0800

    Add contract

---
+++ contract/src/contract.rs
@@ -0,0 +1,125 @@
+use crate::msg::{GreetResp, InstantiateMsg, QueryMsg};
+use crate::state::ADMINS;
+use cosmwasm_std::{
+    to_json_binary, Binary, Deps, DepsMut, Empty, Env, MessageInfo, Response, StdResult,
+};
+
+pub fn instantiate(
+    deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    msg: InstantiateMsg,
+) -> StdResult<Response> {
+    let admins: StdResult<Vec<_>> = msg
+        .admins
+        .into_iter()
+        .map(|addr| deps.api.addr_validate(&addr))
+        .collect();
+    ADMINS.save(deps.storage, &admins?)?;
+
+    Ok(Response::new())
+}
+
+pub fn query(deps: Deps, _env: Env, msg: QueryMsg) -> StdResult<Binary> {
+    use QueryMsg::*;
+
+    match msg {
+        Greet {} => to_json_binary(&query::greet()?),
+        AdminsList {} => to_json_binary(&query::admins_list(deps)?),
+    }
+}
+
+
+mod query {
+    use crate::msg::AdminsListResp;
+
+    use super::*;
+
+    pub fn greet() -> StdResult<GreetResp> {
+        let resp = GreetResp {
+            message: "Hello World".to_owned(),
+        };
+
+        Ok(resp)
+    }
+
+    pub fn admins_list(deps: Deps) -> StdResult<AdminsListResp> {
+        let admins = ADMINS.load(deps.storage)?;
+        let resp = AdminsListResp { admins };
+        Ok(resp)
+    }
+}
+
+#[allow(dead_code)]
+pub fn execute(
+    _deps: DepsMut,
+    _env: Env,
+    _info: MessageInfo,
+    _msg: Empty
+) -> StdResult<Response> {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+    use cosmwasm_std::Addr;
+    use cw_multi_test::{App, ContractWrapper, Executor};
+
+    use crate::msg::AdminsListResp;
+
+    use super::*;
+
+    #[test]
+    fn instantiation() {
+        let mut app = App::default();
+
+        let code = ContractWrapper::new(execute, instantiate, query);
+        let code_id = app.store_code(Box::new(code));
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg { admins: vec![] },
+                &[],
+                "Contract",
+                None,
+            )
+            .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(addr, &QueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(resp, AdminsListResp { admins: vec![] });
+
+        let admin1 = app.api().addr_make("admin1").to_string();
+        let admin2 = app.api().addr_make("admin2").to_string();
+
+        let addr = app
+            .instantiate_contract(
+                code_id,
+                Addr::unchecked("owner"),
+                &InstantiateMsg {
+                    admins: vec![admin1.clone(), admin2.clone()],
+                },
+                &[],
+                "Contract 2",
+                None,
+            )
+            .unwrap();
+
+        let resp: AdminsListResp = app
+            .wrap()
+            .query_wasm_smart(addr, &QueryMsg::AdminsList {})
+            .unwrap();
+
+        assert_eq!(
+            resp,
+            AdminsListResp {
+                admins: vec![Addr::unchecked(admin1), Addr::unchecked(admin2)],
+            }
+        );
+    }
+}
