c744a92fd006b76fc39a4bff889bcc3e1eb6d3a4
Chiu Lam <happylam23@gmail.com>
2025-04-10 13:49:39 +0800

    Remove deployment files

--- contracts/deployer/config/dukong/dapp_template.ts
+++ contracts/deployer/config/dukong/dapp_template.ts
@@ -5,7 +5,7 @@ export const deploymentConfig: DeploymentConfig = {
   "checksumsPath": "../artifacts/checksums.txt",
   "label": "My Dapp Template",
   "initMsg": {
-    "count": 8
+    "count": 0
   },
   "saveDeployment": true
 };
@@ -13,7 +13,7 @@ export const deploymentConfig: DeploymentConfig = {
 export const migrationConfig: MigrationConfig = {
   "name": "dapp_template",
   "network": "dukong",
-  "contractAddress": "mantra15wev5zt4d733d2yskl55vh4a4gutalqdud6h2tygdqtxqh7z87pshu8nxq",
+  "contractAddress": "your_contract_address_here", // Replace with the actual contract address after deployment
   "contractWasmPath": "../artifacts/dapp_template.wasm",
   "checksumsPath": "../artifacts/checksums.txt",
   "migrateMsg": {},
