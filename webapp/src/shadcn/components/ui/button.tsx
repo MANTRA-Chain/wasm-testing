65b3266be8d49de19e1d862ea59ebd6f91dfbae9
Chiu Lam <happylam23@gmail.com>
2025-04-20 02:16:57 +0800

    fix button hover

--- webapp/src/shadcn/components/ui/button.tsx
+++ webapp/src/shadcn/components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-zinc-100 text-zinc-900 hover:bg-zinc-100/80 dark:bg-zinc-800 dark:text-zinc-50 dark:hover:bg-zinc-800/80",
         ghost: "hover:bg-zinc-100 hover:text-zinc-900 dark:hover:bg-zinc-800 dark:hover:text-zinc-50",
         link: "text-zinc-900 underline-offset-4 hover:underline dark:text-zinc-50",
-        primary: "bg-zinc-900 text-zinc-50 hover:bg-zinc-900/90 dark:bg-base-primary dark:text-base-primary-foreground dark:hover:bg-custom-colors-primary-500/90",
+        primary: "bg-zinc-900 text-zinc-50 hover:bg-zinc-900/90 dark:bg-base-primary dark:text-base-primary-foreground dark:hover:bg-base-primary/90",
       },
       size: {
         default: "h-10 px-4 py-2",
