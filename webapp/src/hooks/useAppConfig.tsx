de65af861ddb5456368a460fec647a3c780e6112
Chiu Lam <happylam23@gmail.com>
2025-04-22 17:32:26 +0800

    No contract address for mainnet

--- webapp/src/hooks/useAppConfig.tsx
+++ webapp/src/hooks/useAppConfig.tsx
@@ -7,10 +7,7 @@ export const useAppConfig = () => {
 
   switch (chainName) {
     case MantraChain.Mainnet:
-      return {
-        contractAddress:
-          'mantra1c4darky93xxfseg95vpvn55cul9uf5raza5qsrfzwk2pmue6xctsfpws3f',
-      };
+      throw new Error('Mainnet is not supported yet');
     case MantraChain.Testnet:
       return {
         contractAddress:
