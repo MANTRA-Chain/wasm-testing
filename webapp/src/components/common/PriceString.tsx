b3e93fa29dd7198bd59ffce50557ea1005f2a04c
pear25 <piersonlimas12@gmail.com>
2025-05-09 23:57:13 +0800

    feat: price string component

---
+++ webapp/src/components/common/PriceString.tsx
@@ -0,0 +1,56 @@
+import { cva } from 'class-variance-authority';
+
+import { cn } from '@/shadcn/lib/utils';
+
+type Props = {
+  amount: string;
+  state?: 'positive' | 'negative' | 'default' | 'warning';
+  prefix?: string;
+  suffix?: string;
+  symbol?: string;
+  className?: string;
+};
+
+const priceStringVariants = cva('font-mono inline-flex items-center gap-1.5', {
+  variants: {
+    state: {
+      default: 'text-base-foreground',
+      warning: 'text-base-warning',
+      negative: 'text-base-destructive',
+      positive: 'text-base-positive',
+    },
+  },
+});
+
+const fractionalVariants = cva('', {
+  variants: {
+    state: {
+      default: 'text-base-muted-foreground',
+      warning: 'text-base-warning-muted-foreground',
+      negative: 'text-base-destructive-muted-foreground',
+      positive: 'text-base-positive-muted-foreground',
+    },
+  },
+});
+
+export const PriceString: React.FC<Props> = ({
+  amount,
+  state = 'default',
+  prefix,
+  suffix,
+  symbol,
+  className,
+}) => {
+  const [integer, fraction] = amount.split('.');
+  return (
+    <div className={cn(className, priceStringVariants({ state }))}>
+      {symbol && <div>{symbol}</div>}
+      {prefix && <div>{prefix}</div>}
+      <div>
+        <span>{integer}</span>
+        <span className={cn(fractionalVariants({ state }))}>.{fraction}</span>
+      </div>
+      {suffix && <div>{suffix}</div>}
+    </div>
+  );
+};
